51640a66ff012812ea8f9fedb9b389dec8e41ac7
Mats Melander <mats.melander@gmail.com>
2015-10-04 22:38:36 +0200

    Collect when 1 min past the hour

--- py/B593.py
+++ py/B593.py
@@ -167,7 +167,7 @@ class Router():
             return
 
         if interval > 0 and isfunction(function):
-            # delay until 1 minute past the hour
+            # delay until 1 minute past the hour 
             t = datetime.datetime.now()
             delay = (t.replace(t.year, t.month, t.day, t.hour+1, 2, 0, 0) - t).total_seconds()
             time.sleep(delay)
